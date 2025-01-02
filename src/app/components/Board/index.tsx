d1d4103b0b0d1b3dee0d2e6763a52379013c356a
Douglas Fanucchi <douglasfanucchi@gmail.com>
2025-01-02 15:20:05 -0300

    refact: removes hardcoded board and generates it with loops

--- src/app/components/Board/index.tsx
+++ src/app/components/Board/index.tsx
@@ -23,21 +23,17 @@ export function Board(
 
     return <div className="board">
         <h1>{header}</h1>
-        <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => onPlay(0)} />
-            <Square value={squares[1]} onSquareClick={() => onPlay(1)} />
-            <Square value={squares[2]} onSquareClick={() => onPlay(2)} />
-        </div>
-        <div className="board-row">
-            <Square value={squares[3]} onSquareClick={() => onPlay(3)} />
-            <Square value={squares[4]} onSquareClick={() => onPlay(4)} />
-            <Square value={squares[5]} onSquareClick={() => onPlay(5)} />
-        </div>
-        <div className="board-row">
-            <Square value={squares[6]} onSquareClick={() => onPlay(6)} />
-            <Square value={squares[7]} onSquareClick={() => onPlay(7)} />
-            <Square value={squares[8]} onSquareClick={() => onPlay(8)} />
-        </div>
+        {[1, 2, 3].map((_, rowIndex) => 
+            <div key={rowIndex} className="board-row">
+                {[1, 2, 3].map((_, index) =>
+                    <Square
+                        key={index}
+                        value={squares[index + rowIndex * 3]}
+                        onSquareClick={() => onPlay(index + rowIndex * 3)}
+                    />
+                )}
+            </div>
+        )}
     </div>
 }
 
